970a4c292d69a32c9fcc2fed034816b12d8d0717
Inusha <inusha12345@gmail.com.com>
2023-12-09 17:12:38 -0330

    cgj

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char* argv[]) {
     SDL_Renderer* renderer = NULL;
     SDL_Event event;
     SDL_Rect square = { 400, 300, 50, 50 }; // Initial position and size of the square
+    int vx = 0.5, vy = 0.5; // Initial velocity of the square
 
     // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -13,7 +14,7 @@ int main(int argc, char* argv[]) {
     }
 
     // Create window
-    window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1600, 1200, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 2400, 1800, SDL_WINDOW_SHOWN);
     if (window == NULL) {
         SDL_Log("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         return 1;
@@ -35,13 +36,21 @@ int main(int argc, char* argv[]) {
             }
             else if (event.type == SDL_KEYDOWN) {
                 switch (event.key.keysym.sym) {
-                case SDLK_UP:    square.y -= 10; break;
-                case SDLK_DOWN:  square.y += 10; break;
-                case SDLK_LEFT:  square.x -= 10; break;
-                case SDLK_RIGHT: square.x += 10; break;
+                case SDLK_UP:    vy -= 1; break;
+                case SDLK_DOWN:  vy += 1; break;
+                case SDLK_LEFT:  vx -= 1; break;
+                case SDLK_RIGHT: vx += 1; break;
                 }
             }
         }
+
+        // Update square position
+        square.x += vx;
+        square.y += vy;
+
+        // Check for collision with window boundaries
+        if (square.x < 0 || square.x + square.w > 2400) vx = -vx;
+        if (square.y < 0 || square.y + square.h > 1800) vy = -vy;
 
         // Clear screen
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
